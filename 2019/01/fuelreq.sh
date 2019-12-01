2236edb83f92386662d15ab9500ec7a8aec5516f
Patrik Sandorfy <sandorfy.patrik@adevinta.com>
2019-12-01 11:21:20 +0100

    Add recursion to calculate fuel for fuel's mass

--- 2019/01/fuelreq.sh
+++ 2019/01/fuelreq.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+function fuelForFuel () {
+	local mass=$1
+	local fuelNeeded=$((($mass/3)-2))
+	if [[ "$fuelNeeded" -ge 0 ]]
+	then
+		fuelSum=$(($fuelSum+$fuelNeeded))
+		fuelForFuel $fuelNeeded
+	fi
+}
+
 fuelSum=0
 while read mass
 do
 	fuelNeeded=$((($mass/3)-2))
+	fuelForFuel $fuelNeeded
 	fuelSum=$(($fuelSum+$fuelNeeded))
 done < input.txt
 
 echo $fuelSum
+
